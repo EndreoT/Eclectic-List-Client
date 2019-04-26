90634044b7ed81570836f4e0f8ead5fa3287d81e
Tristan Endreo <endreotm@gmail.com>
2019-04-26 11:55:26 -0700

    added history fix to server.js

--- server.js
+++ server.js
@@ -5,9 +5,19 @@ const path = require('path');
 const serveStatic = require('serve-static');
 
 const app = express();
-app.use(serveStatic(`${__dirname  }/dist`));
+
+app.use(serveStatic(`${__dirname}/dist`));
+
+app.use(history({
+    disableDotRule: true,
+    verbose: true,
+}));
+
+app.get('/', function (req, res) {
+    res.render(path.join(`${__dirname}/dist/index.html`));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log(`server started ${ port}`);
+console.log(`server started ${port}`);
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,47 +1,39 @@
 <template>
   <div class="container" id="page-content">
-
     <PageTitle :pageTitle="pageTitle"></PageTitle>
 
     <!-- All users -->
-    <UnorderedList
-      :view="view"
-      :list="users"
-      :listName="listName"
-      :totalItems="totalUsers"
-    ></UnorderedList>
+    <UnorderedList :view="view" :list="users" :listName="listName" :totalItems="totalUsers"></UnorderedList>
 
     <!-- <div v-if="error">
       <h1>{{error}}</h1>
-    </div> -->
-    
-    <ResourceRetrievalError v-if="error" :error="error"></ResourceRetrievalError>  
+    </div>-->
 
+    <ResourceRetrievalError v-if="error" :error="error"></ResourceRetrievalError>
   </div>
 </template>
 
 <script>
-
-import {getUsers} from "../API/API"
+import { getUsers } from "../API/API";
 import ResourceRetrievalError from "../components/ResourceRetrievalError";
 import UsersItemView from "../components/UsersItemView";
 import UnorderedList from "../components/UnorderedList";
 import PageTitle from "../components/PageTitle";
 
 export default {
-  name: 'users',
+  name: "users",
   components: { PageTitle, ResourceRetrievalError, UnorderedList },
   data: () => ({
     users: [],
     pageTitle: "All Users",
     listName: "users",
     view: UsersItemView,
-    error: '',
+    error: ""
   }),
   computed: {
     totalUsers() {
       return this.users.length;
-    },
+    }
   },
   async mounted() {
     try {
@@ -51,9 +43,9 @@ export default {
       } else {
         this.error = response.data.message;
       }
-    }catch (error) {
-      console.log(error)
+    } catch (error) {
+      console.log(error);
     }
-  },
+  }
 };
 </script>
