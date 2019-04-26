69a0ae561f9468c7faf0a946b95bac421e26a98b
Tristan Endreo <endreotm@gmail.com>
2019-04-26 12:23:46 -0700

    updating server.js for static serving

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const history = require('connect-history-api-fallback');
 
 const app = express();
 
-app.use(serveStatic(`${__dirname}/dist`));
+const staticFileMiddleware = express.static(path.join(`${__dirname}/dist`));
 
+app.use(staticFileMiddleware);
 app.use(history({
-    disableDotRule: true,
-    verbose: true,
+  disableDotRule: true,
+  verbose: true
 }));
+app.use(staticFileMiddleware);
 
 app.get('/', function (req, res) {
     res.render(path.join(`${__dirname}/dist/index.html`));
